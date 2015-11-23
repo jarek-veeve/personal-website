01139d3426b0033987491a44613e8d16be292404
jarek <jaroslaw.jasinski89@gmail.com>
2015-11-23 20:42:08 +0000

    Added delay to contact button

--- src/javascript/scripts.js
+++ src/javascript/scripts.js
@@ -2,6 +2,13 @@
  * Created by Jarek on 14/10/2015.
  */
 $(document).ready(function(){
+
+
+   function show_contact(){
+       $('.contact-button-container').fadeIn(2000).css('display', 'inline');
+   }
+    setTimeout(show_contact, 3000)
+
     $('a[href^="#"]').on('click',function (e) {
         e.preventDefault();
 
@@ -22,6 +29,14 @@ $(document).ready(function(){
     }, function(){
         $(this).css('box-shadow', '0px 0px 2px 7px #848484');
     })
+/*
+    $(function(){
+
+        $(".contact-button-container").addClass("contact-button-container-slide");
+        console.log($(".contact-button-container"));
+
+    });*/
+
 });
 
 
