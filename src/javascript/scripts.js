842986a4005f4f09ea950cf0a9fbec015c01120e
jarek <jaroslaw.jasinski89@gmail.com>
2015-11-30 21:35:34 +0000

    Added welcome box and text

--- src/javascript/scripts.js
+++ src/javascript/scripts.js
@@ -2,12 +2,9 @@
  * Created by Jarek on 14/10/2015.
  */
 $(document).ready(function(){
-
-
-   function show_contact(){
-       $('.contact-button-container').fadeIn(2000).css('display', 'inline');
-   }
-    setTimeout(show_contact, 3000)
+    setTimeout(show_contact, 4000)
+    setTimeout(show_text, 2000)
+    setTimeout(show_welcome_text, 2000)
 
     $('a[href^="#"]').on('click',function (e) {
         e.preventDefault();
@@ -22,21 +19,36 @@ $(document).ready(function(){
         });
     });
 
-
-
     $('.img_circle').hover(function(){
         $(this).css('box-shadow', '0px 0px 2px 7px #173e43');
     }, function(){
         $(this).css('box-shadow', '0px 0px 2px 7px #848484');
     })
-/*
-    $(function(){
-
-        $(".contact-button-container").addClass("contact-button-container-slide");
-        console.log($(".contact-button-container"));
-
-    });*/
-
 });
+
+
+function show_contact(){
+    $('.contact-button-container').fadeIn(2000).css('display', 'inline');
+}
+
+function show_welcome_text(){
+    $('.welcome-text').slideDown(1300).css('display', 'inherit');
+}
+
+function show_text(){
+    $('#server').text('Client');
+    setTimeout(show_text2, 800);
+    //show_text2();
+}
+
+function show_text2(){
+    $('#server').text('Server');
+    setTimeout(show_text, 800);
+}
+
+
+
+
+
 
 
